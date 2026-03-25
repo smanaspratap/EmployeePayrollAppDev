562946154eddb79c73da606c8bb67b5cbac28f34
smanaspratap <manaspratap2003@gmail.com>
2026-03-25 16:33:07 +0530

    [Manas]: Section 2 UC1 - Introduce basic DTO and Model objects

---
+++ src/main/java/com/employeepayroll/dto/EmployeePayrollDTO.java
@@ -0,0 +1,19 @@
+﻿package com.employeepayroll.dto;
+
+/**
+ * Data Transfer Object for Employee Payroll request data.
+ * Simplistic design with name and salary fields initially.
+ */
+public class EmployeePayrollDTO {
+    public String name;
+    public long salary;
+
+    // Parameterized constructor
+    public EmployeePayrollDTO(String name, long salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    
+    // Default constructor
+    public EmployeePayrollDTO() {}
+}
