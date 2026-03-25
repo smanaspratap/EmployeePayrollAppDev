1a65cb85df0493ca5227c024e4e5ce3c8bc030c7
smanaspratap <manaspratap2003@gmail.com>
2026-03-25 16:32:59 +0530

    [Manas]: Section 1 UC1 - Setup base Spring Boot project structure and pom.xml

---
+++ src/main/java/com/employeepayroll/EmployeePayrollAppApplication.java
@@ -0,0 +1,17 @@
+﻿package com.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class for the Employee Payroll Application.
+ * Initializes the Spring Boot context.
+ */
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+    
+    // Main method to run the Spring Boot app
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+    }
+}
