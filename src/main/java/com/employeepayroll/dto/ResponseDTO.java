db2f5b7bcad2729a0ad9ec6f44f05f1a7b7246c0
smanaspratap <manaspratap2003@gmail.com>
2026-03-25 16:33:27 +0530

    [Manas]: Create standardized ResponseDTO wrapper for generic API responses

---
+++ src/main/java/com/employeepayroll/dto/ResponseDTO.java
@@ -0,0 +1,17 @@
+﻿package com.employeepayroll.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Standardized wrapper for API responses.
+ * Provides a common structure for success and error payloads.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDTO {
+    private String message;
+    private Object data;
+}
